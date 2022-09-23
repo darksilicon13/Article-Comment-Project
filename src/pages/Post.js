971370a4c00d43d9f98adc641ad51f56d8458365
darksilicon13 <darksilicon@outlook.com>
2022-09-23 12:45:20 +0900

    delete post button made

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {useNavigate, useParams} from 'react-router-dom';
-import { selectPostById } from '../redux/modules/postsSlice';
+import { deletePost, selectPostById } from '../redux/modules/postsSlice';
 
 const PostPage = () => {
     const {id} = useParams();
 
     const navigate = useNavigate();
 
+    const dispatch = useDispatch();
+
     const post = useSelector(state=>selectPostById(state, id))
 
     return (
         <article>
+            <button onClick={()=>navigate('/')}>Home</button><br/>
             <code>{post.id}</code>
             <h1>{post.title}</h1>
             <p>{post.content}</p>
             <p>{post.date}</p>
             <button onClick={()=>navigate(`/editpost/${id}`)}>Edit</button>
+            <button onClick={()=> {
+                dispatch(deletePost({id}));
+                navigate('/');
+            }}>delete</button>
         </article>
     );
 }
